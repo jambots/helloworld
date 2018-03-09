15770098b0428a5a858379c35a13d9537802018a
Terrance Schubring <tschubring@gmail.com>
2018-03-09 14:28:16 -0600

    publish test

--- pages.js
+++ pages.js
@@ -176,8 +176,8 @@ var pages=[
     {"name":"menu", "top":0, "left":0, "height":8, "width":8}
   ],
   "contents":[
-    {"name":"faqsDiv", "top":7.5, "left":0, "height":50, "width":48, "lineHeight":2.6, "fontSize":2, "fontFamily":"akzibold", "color":"white", "textAlign":"left", "defaultText":"How do I become a rich and<br>famous Radio K DJ?"},
-    {"name":"regentsDiv", "top":57.5, "left":0, "height":8, "width":48, "lineHeight":2.6, "fontSize":2, "fontFamily":"akzibold", "color":"white", "textAlign":"left", "defaultText":"How do I become a rich and<br>famous Radio K DJ?"}
+    {"name":"faqsDiv", "top":8, "left":0, "height":42, "width":48, "lineHeight":2.6, "fontSize":2, "fontFamily":"akzibold", "color":"white", "textAlign":"left", "defaultText":"How do I become a rich and<br>famous Radio K DJ?"},
+    {"name":"regentsDiv", "top":50, "left":0, "height":8, "width":48, "lineHeight":2.6, "fontSize":2, "fontFamily":"akzibold", "color":"white", "textAlign":"left", "defaultText":"How do I become a rich and<br>famous Radio K DJ?"}
   ]
 },
 {"name":"contact", "vAlign":"middle", "hideLeft":0, "hideTop":0, "hideLeftMargin":0, "hideTopMargin":0, "hideOpac":0, "height":58,
